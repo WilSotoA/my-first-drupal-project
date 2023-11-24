9748a75dedc73ae12be35e8bb46a892e6c15c13c
WilSotoA <wilmerandressotoalmeida@gmail.com>
2023-11-24 09:37:59 -0500

    add-terms-and-privacy-pages-and-menu-unit-7

---
+++ web/sites/default/files/php/twig/655e5f6316857_block-content-add-list.ht_BpC8eDoUL21ZeHipPQbbNIPbH/P9Yd1Atxmp-dKo3cxZ9kCHT1ma3MI6hPhhd8_Ryi4Dk.php
@@ -0,0 +1,93 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/contrib/gin/templates/admin/block-content-add-list.html.twig */
+class __TwigTemplate_77efd3b730e09290f7b4949ff0fc3ca8 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doGetParent(array $context)
+    {
+        // line 20
+        return "@gin/admin/entity-add-list.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $this->parent = $this->loadTemplate("@gin/admin/entity-add-list.html.twig", "themes/contrib/gin/templates/admin/block-content-add-list.html.twig", 20);
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/contrib/gin/templates/admin/block-content-add-list.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  36 => 20,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "themes/contrib/gin/templates/admin/block-content-add-list.html.twig", "/var/www/html/web/themes/contrib/gin/templates/admin/block-content-add-list.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array();
+        static $filters = array();
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                [],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
